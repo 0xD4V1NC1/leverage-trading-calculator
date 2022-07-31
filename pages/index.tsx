964c780f53672ca0a80eb3538122ee625cf27271
0xD4V1NC1 <0xD4V1NC1@gmail.com>
2022-07-31 16:53:32 -0400

    more base changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react';
 import type { NextPage } from 'next';
+import Layout from '../components/Layout/Layout';
 
-const Home: NextPage = () => (
-  <div>
-    <h2>HELLO WORLD</h2>
-  </div>
-);
+const Home: NextPage = () => {
+  const [leverage, setLeverage] = useState<number>(0);
+  const [collateral, setCollateral] = useState<number>(0);
+  const [entryPrice, setEntryPrice] = useState<number>(0);
+  const [exitPrice, setExitPrice] = useState<number>(0);
+  const [tradeDirection, setTradeDirection] = useState<'long' | 'short'>('long');
+  return (
+    <Layout>
+      <div>
+        <h2 className="text-red-500">Hello World</h2>
+      </div>
+    </Layout>
+
+  );
+};
 
 export default Home;
