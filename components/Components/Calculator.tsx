0703194c53d302c31333c910a0f5faf33dec2ef7
0xD4V1NC1 <0xD4V1NC1@gmail.com>
2022-08-09 12:23:53 -0400

    remove staging todo

--- components/Components/Calculator.tsx
+++ components/Components/Calculator.tsx
@@ -78,10 +78,8 @@ const Calculator = () => {
       setAssetPercentIncrease(percentIncreaseNeeded);
     }
   };
-  // @TODO remove that p tag
   return (
     <div className="flex flex-col w-full">
-      <p> STAGING TEST</p>
       <Formik
         initialValues={{
           direction: 'long',
