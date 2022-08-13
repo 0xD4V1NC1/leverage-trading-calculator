6b5b7144e5c27f1cdc539b615f81e14103c3fc3b
0xD4V1NC1 <0xD4V1NC1@gmail.com>
2022-08-13 18:12:43 -0400

    stashing changes... still need to fix values bug

--- components/Components/Calculator.tsx
+++ components/Components/Calculator.tsx
@@ -10,6 +10,7 @@ type Direction = 'short' | 'long';
 
 const CalculatorSchema = Yup.object().shape({
   direction: Yup.mixed<Direction>().oneOf(['short', 'long']).required('This field is required'),
+  // isLongTrade: Yup.boolean().required('This field is required'),
   collateral: Yup.number().positive().typeError('you must specify a number').required('This field is required'),
   currentPrice: Yup.number().positive().typeError('you must specify a number').required('This field is required'),
   willingToRiskPercentage: Yup.number().positive().typeError('you must specify a number').required('This field is required'),
@@ -17,17 +18,30 @@ const CalculatorSchema = Yup.object().shape({
   riskToRewardRatio: Yup.number().positive().typeError('you must specify a number').required('This field is required'),
 });
 
-// const FormLabel = () => ();
-const Result = ({ text, number, isPercent }: { text: string, number: any, isPercent?: boolean }) => (
+const Result = ({
+  text, number, isPercent, numberClasses,
+}: { text: string, number: any, isPercent?: boolean, numberClasses?: string }) => (
   <div className="flex">
-    <p className="">{text}</p>
-    <p className="">
+    <p>{text}</p>
+    <p className={`${numberClasses || ''}`}>
       &nbsp;
+      {isPercent ? '' : '$'}
       {number.toFixed(2)}
       {isPercent ? '%' : ''}
     </p>
   </div>
 );
+
+/* <div id="direction-group" className={sectionGroupClasses}>
+          <label className={labelClasses} htmlFor="direction">
+            Direction
+          </label>
+          <Field className={fieldClasses} as="select" name="direction">
+            <option value="short">Short</option>
+            <option value="long">Long</option>
+          </Field>
+          <ErrorMessage component="a" className={errorMsgClasses} name="direction" />
+        </div> */
 
 const Calculator = () => {
   const [stopPrice, setStopPrice] = useState<number>(0);
@@ -35,18 +49,19 @@ const Calculator = () => {
   const [maxLost, setMaxLost] = useState<number>(0);
   const [maxProfit, setMaxProfit] = useState<number>(0);
   const [assetPercentChange, setAssetPercentChange] = useState<number>(0);
-  const [tradeDirection, setTradeDirection] = useState<Direction>('long');
+  const [isLong, setIsLong] = useState<boolean>(false);
 
   const sectionGroupClasses = 'grid grid-col-2 mb-4';
-  const labelClasses = 'text-2xl text-semibold text-black dark:text-white mr-4';
+  const labelClasses = 'text-2xl text-semibold text-black dark:text-white mr-4 mb-4';
   const fieldClasses = 'rounded-lg p-2';
   const errorMsgClasses = 'text-red-500';
+  // const tradeDirection = isLong ? 'long' : 'short';
 
   const doCalculations = (values:any) => {
+    console.log('Values (doCalculations): ', values);
     const {
       direction, collateral, willingToRiskPercentage, currentPrice, leverage, riskToRewardRatio,
-    } = values; // long, 100, $10 or 10%, 1000, 40, 2
-    setTradeDirection(direction);
+    } = values;
 
     const totalDollarValue = (collateral * leverage); // 4000
     const sharesCanBuy = (collateral * leverage) / currentPrice; // 4 shares
@@ -66,7 +81,7 @@ const Calculator = () => {
       setMaxLost(maxPossibleLost);
       setMaxProfit(maxPossibleProfit);
       setAssetPercentChange(percentIncreaseNeeded);
-    } else if (direction === 'short') {
+    } else {
       const stopLostPrice = currentPrice + (willingToRiskPercentage * perPercentRate); // 997.5 --> which would be a $10 and 10% lost... (@TODO is this dollar or percent?)
       const targetProfit = (willingToRiskPercentage * riskToRewardRatio); // WTR $10 and RtR is 2... so 10 * 2
       const takeProfitPrice = currentPrice - (targetProfit * perPercentRate);
@@ -78,102 +93,130 @@ const Calculator = () => {
       setAssetPercentChange(percentDecreaseNeeded);
     }
   };
+
+  console.log('tradeDirection', isLong);
   return (
     <div className="flex flex-col w-full">
       <Formik
         initialValues={{
-          direction: 'long',
+          direction: isLong ? 'long' : 'short',
           collateral: 0,
           willingToRiskPercentage: 0,
           currentPrice: 0,
           leverage: 0,
           riskToRewardRatio: 0,
         }}
-        onSubmit={(values) => doCalculations(values)}
+        onSubmit={(values) => {
+          console.log('shit was submitted');
+          doCalculations(values);
+        }}
         validationSchema={CalculatorSchema}
       >
-        <Form className="flex flex-col justify-center bg-gray-100 dark:bg-[#121212] shadow-2xl dark:shadow-[#222] p-4 rounded-lg mx-auto w-4/5 md:w-1/2 border-[1px] border-black dark:border-white">
-          <div id="direction-group" className={sectionGroupClasses}>
-            <label className={labelClasses} htmlFor="direction">
-              Direction
-            </label>
-            {/* <Field className={fieldClasses} id="direction" name="direction" /> */}
-            <Field className={fieldClasses} as="select" name="direction">
-              <option value="short">Short</option>
-              <option value="long">Long</option>
-            </Field>
-            <ErrorMessage component="a" className={errorMsgClasses} name="direction" />
-          </div>
+        {({ setFieldValue, values }) => (
+          <Form className="flex flex-col justify-center bg-gray-100 dark:bg-[#121212] shadow-2xl dark:shadow-[#222] p-4 rounded-lg mx-auto w-4/5 md:w-1/2">
 
-          <div id="collateral-group" className={sectionGroupClasses}>
-            <label className={labelClasses} htmlFor="collateral">
-              Collateral
-            </label>
-            <Field className={fieldClasses} id="collateral" name="collateral" type="number" />
-            <ErrorMessage
-              component="a"
-              className={errorMsgClasses}
-              name="collateral"
-            />
-          </div>
+            <div id="direction-group" className={sectionGroupClasses}>
+              <label className={labelClasses} htmlFor="direction"> Trade Direction </label>
+              <div className="flex w-full">
+                <button
+                  type="button"
+                  name="direction"
+                  // @TODO talk to Tim... why do i have to do inverse logic for this to work
+                  value={isLong ? 'short' : 'long'}
+                  onClick={(e) => {
+                    const { value } = e.target as HTMLTextAreaElement;
+                    console.log('toggle clicked');
+                    console.log('value', value);
+                    console.log('direction', values.direction);
 
-          <div id="willing-to-risk-in-dollar-group" className={sectionGroupClasses}>
-            <label className={labelClasses} htmlFor="willingToRiskPercentage">
-              Willing to Risk (in %)
-            </label>
-            <Field className={fieldClasses} id="willingToRiskPercentage" name="willingToRiskPercentage" type="number" />
-            <ErrorMessage
-              component="a"
-              className={errorMsgClasses}
-              name="willingToRiskPercentage"
-            />
-          </div>
+                    setIsLong(!isLong);
+                    setFieldValue('direction', value);
+                  }}
+                  className="flex w-full font-bold rounded-lg"
+                >
+                  <div className={`${isLong ? 'bg-green-500 text-black dark:text-white w-6/12' : 'bg-gray-100 text-black dark:bg-[#121212] dark:text-white w-5/12'} rounded-l-lg py-2`}>
+                    <p>Long</p>
+                  </div>
+                  <div className={`w-1/12 border-solid ${isLong ? 'border-l-green-500 border-l-[20px] border-y-transparent border-y-[20px] border-r-0' : 'border-r-red-500 border-r-[20px] border-y-transparent border-y-[20px] border-l-0'} bg-gray-100 dark:bg-[#121212]`} />
 
-          <div id="current-price-group" className={sectionGroupClasses}>
-            <label className={labelClasses} htmlFor="currentPrice">
-              Current Price
-            </label>
-            <Field className={fieldClasses} id="currentPrice" name="currentPrice" type="number" />
-            <ErrorMessage component="a" className={errorMsgClasses} name="currentPrice" />
-          </div>
+                  <div className={`${isLong ? 'bg-gray-100 text-black dark:bg-[#121212] dark:text-white w-5/12' : 'bg-red-500 text-black dark:text-white w-6/12'} rounded-r-lg py-2`}>
+                    <p>Short</p>
+                  </div>
+                </button>
+              </div>
+            </div>
 
-          <div id="leverage-group" className={sectionGroupClasses}>
-            <label className={labelClasses} htmlFor="leverage">
-              Leverage
-            </label>
-            <Field className={fieldClasses} id="leverage" name="leverage" type="number" />
-            <ErrorMessage
-              component="a"
-              className={errorMsgClasses}
-              name="leverage"
-            />
-          </div>
+            <div id="collateral-group" className={sectionGroupClasses}>
+              <label className={labelClasses} htmlFor="collateral">
+                Collateral
+              </label>
+              <Field className={fieldClasses} id="collateral" name="collateral" type="number" />
+              <ErrorMessage
+                component="a"
+                className={errorMsgClasses}
+                name="collateral"
+              />
+            </div>
 
-          <div id="risk-to-reward-ratio-group" className={sectionGroupClasses}>
-            <label className={labelClasses} htmlFor="riskToRewardRatio">
-              Risk to Reward Ratio (i.e. 2 : 1)
-            </label>
-            <Field className={fieldClasses} id="riskToRewardRatio" name="riskToRewardRatio" type="number" />
-            <ErrorMessage
-              component="a"
-              className={errorMsgClasses}
-              name="riskToRewardRatio"
-            />
-          </div>
+            <div id="willing-to-risk-in-percent-group" className={sectionGroupClasses}>
+              <label className={labelClasses} htmlFor="willingToRiskPercentage">
+                Willing to Risk (in %)
+              </label>
+              <Field className={fieldClasses} id="willingToRiskPercentage" name="willingToRiskPercentage" type="number" />
+              <ErrorMessage
+                component="a"
+                className={errorMsgClasses}
+                name="willingToRiskPercentage"
+              />
+            </div>
 
-          <div className="mt-8 flex justify-center">
-            <Button type="submit" className="py-2 px-4 animate-bounce" text="Calculate" ariaLabel="calculate" color="primary" />
-          </div>
-        </Form>
+            <div id="current-price-group" className={sectionGroupClasses}>
+              <label className={labelClasses} htmlFor="currentPrice">
+                Current Price
+              </label>
+              <Field className={fieldClasses} id="currentPrice" name="currentPrice" type="number" />
+              <ErrorMessage component="a" className={errorMsgClasses} name="currentPrice" />
+            </div>
+
+            <div id="leverage-group" className={sectionGroupClasses}>
+              <label className={labelClasses} htmlFor="leverage">
+                Leverage
+              </label>
+              <Field className={fieldClasses} id="leverage" name="leverage" type="number" />
+              <ErrorMessage
+                component="a"
+                className={errorMsgClasses}
+                name="leverage"
+              />
+            </div>
+
+            <div id="risk-to-reward-ratio-group" className={sectionGroupClasses}>
+              <label className={labelClasses} htmlFor="riskToRewardRatio">
+                Risk to Reward Ratio (i.e. 2 : 1)
+              </label>
+              <Field className={fieldClasses} id="riskToRewardRatio" name="riskToRewardRatio" type="number" />
+              <ErrorMessage
+                component="a"
+                className={errorMsgClasses}
+                name="riskToRewardRatio"
+              />
+            </div>
+
+            <div className="mt-8 flex justify-center">
+              <Button type="submit" className="py-2 px-4 animate-pulse" text="Calculate" ariaLabel="calculate" color="primary" />
+            </div>
+          </Form>
+        )}
+
       </Formik>
-      <div className="text-xl md:text-3xl font-bold text-blue-400 mx-auto pt-8">
-        <Result text="Stop Lost: $" number={stopPrice} />
-        <Result text="Take Profit: $" number={takeProfit} />
-        <Result text="Max Possible Lost: $" number={maxLost} />
-        <Result text="Max Possible Profit: $" number={maxProfit} />
-        {tradeDirection === 'long' ? (
-          <Result text="Percent Increase Needed: " number={assetPercentChange} isPercent />
-        ) : <Result text="Percent Decrease Needed: " number={assetPercentChange} isPercent />}
+      <div className="text-lg md:text-2xl font-bold text-black dark:text-white mx-auto p-8 bg-gray-100 dark:bg-[#121212] shadow-2xl dark:shadow-[#222] rounded-lg mt-8 w-4/5 md:w-1/2">
+        <Result text="Stop Lost:" number={stopPrice} />
+        <Result text="Take Profit:" number={takeProfit} />
+        <Result text="Max Possible Lost:" number={maxLost} numberClasses="text-red-500" />
+        <Result text="Max Possible Profit:" number={maxProfit} numberClasses="text-green-500" />
+        {isLong ? (
+          <Result text="Percent Increase Needed: " number={assetPercentChange} numberClasses="text-green-500" isPercent />
+        ) : <Result text="Percent Decrease Needed: " number={assetPercentChange} numberClasses="text-red-500" isPercent />}
       </div>
     </div>
   );
